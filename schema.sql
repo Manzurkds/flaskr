82bd5b620ada0a388a654893c252eade32672f09
Manzur <manzurkds@gmail.com>
2016-09-28 17:21:53 +0530

    my first commit

---
+++ schema.sql
@@ -0,0 +1,16 @@
+drop table if exists entries;
+PRAGMA foreign_keys = ON;
+create table entries (
+	id integer primary key autoincrement,
+	title text not null,
+	time DATETIME DEFAULT CURRENT_TIMESTAMP,
+	'text' text not null	
+);
+
+drop table if exists commentonentries;
+create table commentonentries (
+	id integer primary key autoincrement,
+	comment_id integer,
+	commenttext text not null,
+	foreign key(comment_id) references entries(id)
+);
