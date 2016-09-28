e5150fe45b821730c3eaa5275f195a740bdf08ff
Manzur <manzurkds@gmail.com>
2016-09-28 17:48:23 +0530

    added comment_id

--- flaskr.py
+++ flaskr.py
@@ -62,7 +62,7 @@ def show_entries():
     db = get_db()
     cur = db.execute('select id, title,time, text from entries order by id desc')
     entries = cur.fetchall()
-    cur = db.execute('select commenttext from commentonentries order by id desc')
+    cur = db.execute('select comment_id, commenttext, from commentonentries order by id desc')
     commentonentries = cur.fetchall()
     return render_template('show_entries.html', entries=entries, commentonentries=commentonentries)
 
